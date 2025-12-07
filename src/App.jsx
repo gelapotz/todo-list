affedbc44d98341346a48e183edc015b2cf33a6d
LAPTOP-4TJAVEAO\maria <cmariahangela@gmail.com>
2025-12-06 20:58:09 -0600

    Add App component and Header component with basic structure

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import './App.css';
+import { Header } from './components/Header';
+import { TodoList } from './components/TodoList';
+import { Footer } from './components/Footer';
+import { Card } from './components/Card';   
+
+function App() {
+    
