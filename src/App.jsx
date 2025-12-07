4d5a02909bafd581399f0e49b13b5568b987402c
LAPTOP-4TJAVEAO\maria <cmariahangela@gmail.com>
2025-12-07 11:00:59 -0600

    Added card

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,21 @@ function App() {
         // Header
         <>
             <Header title="Welcome to My Website!" message="Thanks for visiting my site." />
-            {/* Card */}
+            
+            {/* Todo */}
             <TodoList todos={[
                 { id: 1, text: "Finish reading a book before 2026", completed: false },
                 { id: 2, text: "Finish one LinkedIn Learning course", completed: false },
                 { id: 3, text: "Declutter bedroom closet", completed: true }
             ]} />
+
+            {/* Card */}
+            <Card 
+            title="My Card Title"
+            subtitle="My Card Subtitle"
+            content="This is the content of my card."
+            image=""
+            />
         </>
     );
 }
