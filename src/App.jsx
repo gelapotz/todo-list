a9727b6c8b615e4ddf574869fb14dcfd46815c6f
LAPTOP-4TJAVEAO\maria <cmariahangela@gmail.com>
2025-12-07 10:58:27 -0600

    Added comments

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,19 @@ import './App.css';
 import { Header } from './components/Header';
 import { TodoList } from './components/TodoList';
 import { Footer } from './components/Footer';
-import { Card } from './components/Card';   
+import { Card } from './components/Card';
 
 function App() {
     return (
+        // Header
         <>
-        <Header title="Welcome to My Website!" message="Thanks for visiting my site." />
-    
-    <TodoList todos={[
-        { id: 1, text: "Finish reading a book before 2026", completed: false },
-        { id: 2, text: "Finish one LinkedIn Learning course", completed: false },
-        { id: 3, text: "Declutter bedroom closet", completed: true }
-    ]}/>
-    </>
-);
+            <Header title="Welcome to My Website!" message="Thanks for visiting my site." />
+            {/* Card */}
+            <TodoList todos={[
+                { id: 1, text: "Finish reading a book before 2026", completed: false },
+                { id: 2, text: "Finish one LinkedIn Learning course", completed: false },
+                { id: 3, text: "Declutter bedroom closet", completed: true }
+            ]} />
+        </>
+    );
 }
