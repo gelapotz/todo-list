9f13563920d1f040dfca9791d7643bddf52e37d9
LAPTOP-4TJAVEAO\maria <cmariahangela@gmail.com>
2025-12-07 10:49:16 -0600

    Refactor App component to include return statement and render Header

--- src/App.jsx
+++ src/App.jsx
@@ -5,4 +5,8 @@ import { Footer } from './components/Footer';
 import { Card } from './components/Card';   
 
 function App() {
-    
+    return (
+        <><Header title="Welcome to My Website!" message="Thanks for visiting my site."/>
+        </>
+    )
+}
