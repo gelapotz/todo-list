73aa3323da32afcbfd612250afccad2f2644f194
LAPTOP-4TJAVEAO\maria <cmariahangela@gmail.com>
2025-12-07 10:57:00 -0600

    Fix App component return statement (the fragments??) and render TodoList with sample todos

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,14 @@ import { Card } from './components/Card';
 
 function App() {
     return (
-        <><Header title="Welcome to My Website!" message="Thanks for visiting my site."/>
-        </>
-    )
+        <>
+        <Header title="Welcome to My Website!" message="Thanks for visiting my site." />
+    
+    <TodoList todos={[
+        { id: 1, text: "Finish reading a book before 2026", completed: false },
+        { id: 2, text: "Finish one LinkedIn Learning course", completed: false },
+        { id: 3, text: "Declutter bedroom closet", completed: true }
+    ]}/>
+    </>
+);
 }
